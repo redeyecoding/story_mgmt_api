94238c5aeb7828a31702591b9d9e05cb51ebd718
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-02-07 11:19:08 -0500

    fixed config for both http and https servers

--- config.js
+++ config.js
@@ -1,23 +1,34 @@
 /* 
     main api configuration
+    // Default environment mode is STAGING
+
 */
 
-// Default environment mode is staging
+// Dependencies
+const fs = require('fs');
+const httpsCert = fs.readFileSync('./https/cert.pem', 'utf8');
+const httpsKey = fs.readFileSync('./https/key.pem', 'utf8');
 
-// Set enviorment ports
-const httpPort = process.env.NODE_ENV.trim() === 'staging'.trim() ? 3000 : 3001;
-const httpsPort = process.env.NODE_ENV.trim() === 'production'.trim() ? 5000 : 5001;
+
+// Define environment settings
+const envMode = process.env.NODE_ENV === 'staging'.trim() ? 'staging' : 'production'
+const httpPort = process.env.NODE_ENV === 'staging'.trim() ? 3000 : 3001;
+const httpsPort = process.env.NODE_ENV === 'staging'.trim() ? 5000 : 5001;
 
 
 // Initialize configuration object
 const config = {
     httpConfig: {
         'httpPort': httpPort,
-        'envMode': process.env.NODE_ENV
+        'envMode': envMode
     },
     httpsConfig: {
         'httpsPort': httpsPort,
-        'envMode': process.env.NODE_ENV
+        'envMode': envMode,
+        'options': {
+            cert: httpsCert,
+            key: httpsKey
+        }
     }
 };
 
