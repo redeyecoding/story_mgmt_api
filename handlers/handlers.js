a8f7b36b8f1336b9b34a721ae223516ec43dabdf
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-02-07 13:44:35 -0500

    added handlers to test our different requests (success)

--- handlers/handlers.js
+++ handlers/handlers.js
@@ -2,17 +2,60 @@
     Handlers
 */
 
+// Dependencies
+const _data = require('../lib/data');
+
+
 // Handler object
 const handlers = {};
 
-// Users
-handlers.users = (( data, callback ) => {
-    callback(200, 'You\'ve successfully pulled your data!' );
+// Initilize user data processing object
+handlers._userDataProcessing = {};
+
+
+
+
+// User data processes
+handlers._userDataProcessing.post = (( data, callback ) => {
+
+    console.log('POST METHOD SUCCESS!!');
+});
+
+
+handlers._userDataProcessing.get = (( data, callback ) => {
+
+    console.log('GET METHOD SUCCESS!!');
+});
+
+
+handlers._userDataProcessing.put = (( data, callback ) => {
+
+    console.log('PUT METHOD SUCCESS!!');
+});
+
+
+handlers._userDataProcessing.delete = (( data, callback ) => {
+
+    console.log('DELETE METHOD SUCCESS!!');
 });
 
 
 
-// Not Ffound
+// Users
+handlers.users = (( data, callback ) => {
+    // Check for valid inbound request
+    const validRequests = ['get','post','delete', 'put'];
+    const method = data.method;
+
+    if ( validRequests.includes(method) ) {
+        handlers._userDataProcessing[method]( data, callback );
+    } else {
+        callback(400, { 'Error': 'Invalid request' });
+    }
+});
+
+
+// Not Found
 handlers.notFound = (( data, callback ) => {
     callback(404, { 'Error': 'Page not found' });
 });
