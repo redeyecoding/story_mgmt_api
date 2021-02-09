c8fb807e02ce41a3daf533268b1e7d31272c139a
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-02-09 14:02:39 -0500

    added a utility to hash user passwords

--- utils/util.js
+++ utils/util.js
@@ -1,10 +1,16 @@
 /*
     Utilties
 */
+'use strict'
+// Dependencies
+require('../https/config');
+const crypto = require("crypto")
+const secret = process.env.HASH_PASSWORD_SECRET;
 
-const jsonParser = string => JSON.parse(string);
+
 
 // Error utility
+const jsonParser = string => JSON.parse(string);
 const errorUtility = (( statusCode, message, errorType=null ) => {
     // Assemble statusMessage Object
     const errorObject = {
@@ -17,16 +23,43 @@ const errorUtility = (( statusCode, message, errorType=null ) => {
             message: message,
             code: statusCode
         }
-    }
+    };
 
     const jsonResponse = statusCode === 200 ? errorObject.ok : errorObject.error;
     return JSON.stringify(jsonResponse);
 });
 
 
+// Hash password utility
+    // Generate Salt
+const genRandomString = length => {
+    return crypto.randomBytes(Math.ceil(length/2))
+            .toString('hex') /** convert to hexadecimal format */
+            .slice(0,length);   /** return required number of characters */
+};
+
+
+const sha512 = (password, salt) => {
+    const hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+    hash.update(password);
+    const value = hash.digest('hex');
+    return {
+        salt:salt,
+        passwordHash:value
+    };
+};
+
+const generateHashPassword = userpassword => {
+    const salt = genRandomString(16); /** Gives us salt of length 16 */
+    const passwordData = sha512(userpassword, salt);
+    const hash = passwordData.salt += passwordData.passwordHash
+    return hash;
+};
+
 module.exports = {
     'jsonParser': jsonParser,
-    'errorUtility': errorUtility
-}
+    'errorUtility': errorUtility,
+    'generateHashPassword': generateHashPassword
+};
 
 
