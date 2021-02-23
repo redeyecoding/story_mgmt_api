a0eee38f91728746d37e07e50502aa809a08fe3a
Jeffrey McIntyre <redeyecoding@gmail.com>
2021-02-22 23:09:49 -0500

    added additional conditions for http ternary operator

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ const httpsKey = fs.readFileSync('./https/key.pem', 'utf8');
 
 
 // Define environment settings
-const envMode = process.env.NODE_ENV === 'staging'.trim() ? 'staging' : 'production'
+const envMode = process.env.NODE_ENV === 'staging'.trim()  ? 'staging' : 'production'
 const httpPort = process.env.NODE_ENV === 'staging'.trim() ? 3000 : 3001;
-const httpsPort = process.env.NODE_ENV === 'staging'.trim() ? 5000 : 5001;
+const httpsPort = process.env.NODE_ENV === 'staging'.trim() && httpsCert && httpsKey ? 5000 : 5001;
 
 
 // Initialize configuration object
